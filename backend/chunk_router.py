aed55501db3f9ea912d2a4728951e4aa18a4d115
MohdYaseenS <yaseenasjeer2016@gmail.com>
2025-11-15 11:54:44 +0530

    Rename backend.utlis.py to chunk_and_vectorize.py

---
+++ backend/chunk_router.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from chunk_and_vectorize.chunk_and_vectorize import recursive_text_splitter, vectorize_text_chunks, create_faiss_index
+
+chunk_router = APIRouter()
+
+class PDF(BaseModel):
+    text: str
+
+
+@chunk_router.post("/chunk_and_vectorize")
+async def chunk_and_vectorize(data: PDF):
+    chunks = recursive_text_splitter(text = data.text, chunk_size = 1000, overlap = 200)
+    chunk_vectors = vectorize_text_chunks(chunks = chunks)
+    faiss_vector_index = create_faiss_index(vectors = chunk_vectors)
+
+
+
+
+
+
+
