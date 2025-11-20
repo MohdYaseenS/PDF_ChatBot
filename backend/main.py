8d7ef0f4f26b3a22814f3c07b481d94ab2be7390
MohdYaseenS <yaseenasjeer2016@gmail.com>
2025-11-20 12:43:38 +0530

    Update main.py to load the models in app startup rather than in first query

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
+# main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.api.chunk_router import chunk_router
 from backend.api.llm_router import llm_router
 from backend.api.search_router import search_router
+
+# Import app_state so it loads config, LLM, embeddings once
+from backend.core import app_state
 
 app = FastAPI(title="PDF_ChatBot")
 
@@ -20,3 +24,10 @@ app.include_router(llm_router, prefix="/api/llm")
 @app.get("/")
 def root():
     return {"message": "API is up!"}
+
+@app.on_event("startup")
+def startup_event():
+    # trigger initialization
+    _ = app_state.config
+    _ = app_state.llm
+    _ = app_state.embedding_model
